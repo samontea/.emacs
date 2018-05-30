931f79cb9e74351112e3c445b9c61367ea297599
samontea <merciers.merciers@gmail.com>
2018-05-30 17:00:18 -0400

    enable xterm-mouse-mode, add comments

--- samontea-coding.el
+++ samontea-coding.el
@@ -1,10 +1,13 @@
+;; enalbe projectile globaly
 (projectile-global-mode)
 
+;; enable ggtags for c-mode, c++-mode, and java-mode
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
               (ggtags-mode 1))))
 
+;; turn on git gutter+
 (global-git-gutter+-mode t)
 
 (defun vim-new-line-below ()
@@ -15,5 +18,8 @@
 
 ;; o like functionality from vim, really helpful in org-mode
 (global-set-key (kbd "C-o") 'vim-new-line-below)
+
+;; enable mouse input on the terminal
+(xterm-mouse-mode t)
 
 (provide 'samontea-coding)
